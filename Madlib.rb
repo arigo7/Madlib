bc936c00d7de268c72c802c9126ce2bb344a8554
arigo7 <61640195+arigo7@users.noreply.github.com>
2020-02-29 18:10:56 -0800

    Add files via upload

---
+++ Madlib.rb
@@ -0,0 +1,55 @@
+Puts "Welcome to my MadLib program :). Please enter in some information below:"
+
+Puts "Your least favorite programming language:"
+PLanguage = (gets.chomp).capitalize
+
+Puts "Your name:"
+YourName = (gets.chomp).capitalize
+
+Puts "Your crush's name:"  
+TheirName = (gets.chomp).capitalize
+
+Puts "Adjective:" 
+Adjective1 = (gets.chomp).underscore    
+
+Puts "Adverb:" 
+Adverb1 = (gets.chomp).underscore 
+
+Puts "Verb:" 
+Verb1 = (gets.chomp).underscore 
+
+Puts "Time and place:"
+Time = (gets.chomp)
+Place = (gets.chomp).capitalize
+
+Puts "Verb:" 
+Verb2 = gets.chomp
+
+Puts "Number:"
+Number1 = gets.chomp
+
+adjective: dry
+
+food (plural): tacos
+
+noun (plural): packs
+
+verb ending in -ed: ended
+
+noun: jellyfish"
+
+Dear #{YourName},
+
+I am writing you this message in PLanguage because my Spanish 
+is #{Adjective1} so I wanted to show off a little. I cannot tell you how it happened 
+but I have  #{Adverb1} decided to get enough courage and  #{Verb1} you so you would know… So here is my  confession:
+
+ I think it’s about #{TimeAndPlace} I do so and thank you for #{(Verb1.concat)ing} this 
+    message.  I haven’t stopped thinking about the #{Time.convert to number} time we met and about that first night 
+    we spent  #{Verb2} until #{Number1} am in the morning! ___exclamation—. You were wearing 
+    that ___favorite clothing_  you _ adverb_ wear on Sundays when we hang out and you were waiting 
+    for the bus. That day I would have never imagined that you would become my __adjective___ friend. 
+        You are _____adj— at programming and I am great at _____ so I think we are just going to be ______ forever. 
+        I __HOpe__ you think so too and I would _verb_ to know if you feel the same. Are you in?
+
+Your response: Yes or no?
